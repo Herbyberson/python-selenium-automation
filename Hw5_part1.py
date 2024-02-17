eab72048143096d947e96cea9e91d6d4426b2e2f
Herby Berson <Berson.Herby@gmail.com>
2024-02-17 08:50:17 -0500

    additional code added

--- Hw5_part1.py
+++ Hw5_part1.py
@@ -1,3 +1,35 @@
+from selenium.webdriver.common.by import By
+from behave import given, when, then
+from time import sleep
+from selenium.webdriver.support import expected_conditions as EC
+
+
+SEARCH_FIELD = (By.ID, 'search')
+SEARCH_ICON = (By.XPATH, "//button[@data-test='@web/Search/SearchButton']")
+ADD_TO_CART_BUTTON = (By.CSS_SELECTOR, "[id*='addToCartButton']")
+SIDE_NAV_ADD_TO_CART_BUTTON = (By.CSS_SELECTOR, "[data-test='orderPickupButton']")
+VIEW_CART_PAGE = (By.XPATH, "//a[@href='/cart']")
+ORDER_SUMMARY = (By.CSS_SELECTOR, "[data-test='cart-order-summary']")
+SIDE_NAV_BRAND_NAME =(By.CSS_SELECTOR, "h4[class*='styles__StyledHeading']")
+ORDER_NAME = (By.CSS_SELECTOR, "[data-test='cartItem-title']")
+
+@given('Open Target main page')
+def open_target_main(context):
+    context.driver.get('https://www.target.com/')
+
+
+@when('Search for {product}')
+def search_for_product(context, product):
+    context.driver.find_element(*SEARCH_FIELD).send_keys(product)
+    context.driver.find_element(*SEARCH_ICON).click()
+    sleep(6)
+
+
+@when('Click on Add to Cart button')
+def click_on_add_to_cart(context):
+    context.driver.find_element(*ADD_TO_CART_BUTTON).click()
+    sleep(6)
+
 @when('Store product name')
 def store_product_name(context):
     context.wait.until(EC.presence_of_element_located(SIDE_NAV_BRAND_NAME))
@@ -26,5 +58,6 @@ def verify_cart_product(context):
     actual_name = context.driver.find_element(*ORDER_NAME).text
     assert context.product_name == actual_name, f"Expected {context.product_name}, but got {actual_name}"
     sleep(10)
+
 
 
