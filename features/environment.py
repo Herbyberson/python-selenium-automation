cc29ad6a83dc1564a2b1c5de4d095a37f980f886
Herby Berson <Berson.Herby@gmail.com>
2024-02-22 22:12:48 -0500

    adding code to feature steps

--- features/environment.py
+++ features/environment.py
@@ -1,7 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
+from app.application import Application
 
 def browser_init(context):
     """
@@ -15,6 +18,9 @@ def browser_init(context):
 
     context.driver.maximize_window()
     context.driver.implicitly_wait(4)
+    context.wait = WebDriverWait(context.driver, 15)
+    context.app = Application(context.driver)
+
 
 
 def before_scenario(context, scenario):
---
+++ features/steps/main_page_steps.py
@@ -0,0 +1,94 @@
+from selenium.webdriver.common.by import By
+from behave import given, when, then
+from time import sleep
+from selenium.webdriver.support import expected_conditions as EC
+
+
+SEARCH_FIELD = (By.ID, 'search')
+SEARCH_ICON = (By.XPATH, "//button[@data-test='@web/Search/SearchButton']")
+ADD_TO_CART_BUTTON = (By.CSS_SELECTOR, "[id*='addToCartButton']")
+SIDE_NAV_ADD_TO_CART_BUTTON = (By.CSS_SELECTOR, "[data-test='orderPickupButton']")
+VIEW_CART_PAGE = (By.XPATH, "//a[@href='/cart']")
+ORDER_SUMMARY = (By.CSS_SELECTOR, "[data-test='cart-order-summary']")
+SIDE_NAV_BRAND_NAME =(By.CSS_SELECTOR, "h4[class*='styles__StyledHeading']")
+ORDER_NAME = (By.CSS_SELECTOR, "[data-test='cartItem-title']")
+
+@given('Open Target main page')
+def open_target_main(context):
+    context.app.main_page.open_main()
+
+
+@when('Search for {product}')
+def search_for_product(context, product):
+    context.app.header.search_product()
+
+
+@then('Search results for {expected_result} are shown')
+def verify_search_results_correct(context, expected_result):
+    context.app.search_results_page.verify_search_results_correct(expected_result)
+
+
+@then('Page URL has search term {expected_part_url}')
+def verify_search_results_page_url(context, expected_part_url):
+    context.app.search_results_page.verify_search_results_page_url(expected_part_url)
+
+
+LISTINGS = (By.CSS_SELECTOR, "[data-test='@web/site-top-of-funnel/ProductCardWrapper']")
+PRODUCT_TITLE = (By.CSS_SELECTOR, "[data-test='product-title']")
+PRODUCT_IMG = (By.CSS_SELECTOR, "[class*='ProductCardImage']")
+
+
+@then('Verify that every product has a name and an image')
+def verify_product_name(context):
+    # slowing down the browser to see all products
+    context.driver.execute_script("window.scrollBy(0,2000)", "")
+    sleep(6)
+    context.driver.execute_script("window.scrollBy(0,2000)", "")
+
+    all_products = context.driver.find_elements(*LISTINGS)
+
+    for product in all_products:
+
+        title = product.find_element(*PRODUCT_TITLE).text
+        print(title)
+        assert title, 'Product title not shown'
+        product.find_element(*PRODUCT_IMG)
+
+
+
+@when('Click on Add to Cart button')
+def click_on_add_to_cart(context):
+    context.driver.find_element(*ADD_TO_CART_BUTTON).click()
+    sleep(6)
+
+@when('Store product name')
+def store_product_name(context):
+    context.wait.until(EC.presence_of_element_located(SIDE_NAV_BRAND_NAME))
+    context.product_name = context.driver.find_element(*SIDE_NAV_BRAND_NAME).text
+
+
+@when('Confirm Add to Cart button from side navigation')
+def confirm_add_to_cart(context):
+    context.driver.find_element(*SIDE_NAV_ADD_TO_CART_BUTTON).click()
+    sleep(6)
+
+
+@when('Open cart page')
+def open_cart_page(context):
+    context.driver.find_element(*VIEW_CART_PAGE).click()
+
+
+@then('Verify cart has {amount} item(s)')
+def verify_cart_amount(context, amount):
+    summary = context.driver.find_element(*ORDER_SUMMARY).text
+    assert amount in summary, f"Expected 'empty cart', but got {summary}"
+
+
+@then('Verify cart has correct product')
+def verify_cart_product(context):
+    actual_name = context.driver.find_element(*ORDER_NAME).text
+    assert context.product_name == actual_name, f"Expected {context.product_name}, but got {actual_name}"
+    sleep(10)
+
+
+
